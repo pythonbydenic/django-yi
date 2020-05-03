b804cfaa31af41a6eaa5948cb37f6124eae3ae89
pythonbydenic <62197222+pythonbydenic@users.noreply.github.com>
2020-05-04 00:13:00 +0200

    Update my_extras.py

--- learning_templates/basic_app/templatetags/my_extras.py
+++ learning_templates/basic_app/templatetags/my_extras.py
@@ -6,9 +6,9 @@ register = template.Library()
 
 def cut(value,arg):
 
-    "'"
-    this cuts out all values of "arg" from the string!
-    "'"
+    #"'"                                                 i commented this out
+   # this cuts out all values of "arg" from the string!
+   # "'"
     return value.replace(arg,'')
 
 #register.filter('cut',cut)
